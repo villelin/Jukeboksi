11c5d0221f5d7317342c1ac600c3102ec8991c72
ilppi <jonne.leinonen@metropolia.fi>
2017-04-27 11:09:17 +0300

    jees

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	private int jonne;
 	private int mulu;
 	private int nhopäissoihgoi<ghpiäfhpihoäih;
+	private int villelin;
+	ibhowgghoihör
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

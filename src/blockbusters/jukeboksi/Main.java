d47749d986cf3d06e36247c41ad3a2fe6b938a24
ilppi <jonne.leinonen@metropolia.fi>
2017-04-27 11:45:28 +0300

    klbik

--- src/blockbusters/jukeboksi/Main.java
+++ src/blockbusters/jukeboksi/Main.java
@@ -5,8 +5,6 @@ public class Main {
 	private int jonne;
 	private int mulu;
 	private int nhopäissoihgoi<ghpiäfhpihoäih;
-	private int villelin;
-	ibhowgghoihör
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

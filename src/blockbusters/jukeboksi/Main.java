9c5a83b748b81b5ab1fca56b8ecfd3bf70ffde9e
MULULÄPPÄRI\Joona Ikonen <joona.ikonen@metropolia.fi>
2017-04-28 13:16:26 +0300

    no message

--- src/blockbusters/jukeboksi/Main.java
+++ src/blockbusters/jukeboksi/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	private int jonne;
 	private int mulu;
-	private int nhopäissoihgoi<ghpiäfhpihoäih;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

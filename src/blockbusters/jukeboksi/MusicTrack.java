56d117e248d6c8c81a14a93bad2a751a27e2fe88
villelin <villelin@metropolia.fi>
2017-04-28 12:53:54 +0300

    tetristä

--- src/blockbusters/jukeboksi/MusicTrack.java
+++ src/blockbusters/jukeboksi/MusicTrack.java
@@ -2,30 +2,30 @@ package blockbusters.jukeboksi;
 
 public abstract class MusicTrack
 {
-	private static final int AS3 = 233;
-	private static final int C4 = 261;
-	private static final int D4 = 293;
-	private static final int E4 = 329;
-	private static final int F4 = 349;
-	private static final int G4 = 392;
-	private static final int A4 = 440;
-	private static final int B4 = 493;
-	private static final int C5 = 523;
-	private static final int DS5 = 622;
-	private static final int F5 = 698;
-	private static final int FS5 = 739;
-	private static final int G5 = 784;
-	private static final int GS5 = 830;
-	private static final int A5 = 880;
-	private static final int AS5 = 932;
-	private static final int B5 = 987;
-	private static final int C6 = 1046;
-	private static final int CS6 = 1108;
-	private static final int D6 = 1174;
-	private static final int E6 = 1318;
-	private static final int F6 = 1397;
-	private static final int G6 = 1567;
-	private static final int A6 = 1760;
+	protected static final int AS3 = 233;
+	protected static final int C4 = 261;
+	protected static final int D4 = 293;
+	protected static final int E4 = 329;
+	protected static final int F4 = 349;
+	protected static final int G4 = 392;
+	protected static final int A4 = 440;
+	protected static final int B4 = 493;
+	protected static final int C5 = 523;
+	protected static final int DS5 = 622;
+	protected static final int F5 = 698;
+	protected static final int FS5 = 739;
+	protected static final int G5 = 784;
+	protected static final int GS5 = 830;
+	protected static final int A5 = 880;
+	protected static final int AS5 = 932;
+	protected static final int B5 = 987;
+	protected static final int C6 = 1046;
+	protected static final int CS6 = 1108;
+	protected static final int D6 = 1174;
+	protected static final int E6 = 1318;
+	protected static final int F6 = 1397;
+	protected static final int G6 = 1567;
+	protected static final int A6 = 1760;
 	
 	protected int current_note;
 	
--- src/blockbusters/jukeboksi/Tetris.java
+++ src/blockbusters/jukeboksi/Tetris.java
@@ -2,8 +2,58 @@ package blockbusters.jukeboksi;
 
 public class Tetris extends MusicTrack
 {
+	private static final int BEAT = 85;		// biitin pituus = 85 ms
+	
+	public static int[][] notedata = 
+	{
+		{E6,	BEAT-5,		BEAT*4},
+		{B5,	BEAT-5,		BEAT*2},
+		{C6,	BEAT-5,		BEAT*2},
+		{D6,	BEAT-5,		BEAT*2},
+		{E6,	BEAT-5,		BEAT},
+		{D6,	BEAT-5,		BEAT},
+		{C6,	BEAT-5,		BEAT*2},
+		{B5,	BEAT-5,		BEAT*2},
+		{A5,	BEAT-5,		BEAT*4},
+		{A5,	BEAT-5,		BEAT*2},
+		{C6,	BEAT-5,		BEAT*2},
+		{E6,	BEAT-5,		BEAT*4},
+		{D6,	BEAT-5,		BEAT*2},
+		{C6,	BEAT-5,		BEAT*2},
+		{B5,	BEAT-5,		BEAT*4},
+		{B5,	BEAT-5,		BEAT},
+		{B5,	BEAT-5,		BEAT},
+		{C6,	BEAT-5,		BEAT*2},
+		{D6,	BEAT-5,		BEAT*4},
+		{E6,	BEAT-5,		BEAT*4},
+		{C6,	BEAT-5,		BEAT*4},
+		{A5,	BEAT-5,		BEAT*4},
+		{A5,	BEAT-5,		BEAT*8},
+		{D6,	BEAT-5,		BEAT*4},
+		{D6,	BEAT-5,		BEAT*2},
+		{F6,	BEAT-5,		BEAT*2},
+		{A6,	BEAT-5,		BEAT*4},
+		{G6,	BEAT-5,		BEAT*2},
+		{F6,	BEAT-5,		BEAT*2},
+		{E6,	BEAT-5,		BEAT*4},
+		{E6,	BEAT-5,		BEAT*2},
+		{C6,	BEAT-5,		BEAT*2},
+		{E6,	BEAT-5,		BEAT*4},
+		{D6,	BEAT-5,		BEAT*2},
+		{C6,	BEAT-5,		BEAT*2},
+		{B5,	BEAT-5,		BEAT*4},
+		{B5,	BEAT-5,		BEAT},
+		{B5,	BEAT-5,		BEAT},
+		{C6,	BEAT-5,		BEAT*2},
+		{D6,	BEAT-5,		BEAT*4},
+		{E6,	BEAT-5,		BEAT*4},
+		{C6,	BEAT-5,		BEAT*4},
+		{A5,	BEAT-5,		BEAT*4},
+		{A5,	BEAT-5,		BEAT*8},
+	};
+	
 	public int[] getNote()
 	{
-		return new int[3];
+		return notedata[current_note];
 	}
 }

17ad8275458ad55b9cf6bfaca388eb7ff319b327
ilppi <jonne.leinonen@metropolia.fi>
2017-05-02 14:08:15 +0300

    coincountrt

--- src/blockbusters/jukeboksi/CoinCounter.java
+++ src/blockbusters/jukeboksi/CoinCounter.java
@@ -1,5 +1,50 @@
 package blockbusters.jukeboksi;
 
-public class CoinCounter {
+import java.awt.Color;
 
+public class CoinCounter extends Thread {
+	
+	int coins = 0;
+	int credits = 0;
+	private EV3ColorSensor coinReader;
+	
+	public CoinCounter (EV3ColorSensor coinreader) {
+		this.coinReader = coinreader;
+	}
+	
+	getCoins int () {
+		
+		return coins;
+		
+	}
+	
+	getCredits int () {
+
+		return credits;
+		
+	}
+	
+	useCredits boolean () {
+		
+		if (credits > 0) {
+			credits = credits - 1;
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	run() {
+		
+		while (true){
+			
+			if (!coinReader = Color.GREEN) {
+				coins++;
+				credits++;
+			}
+			sleep 50;
+		}
+		
+	}
 }

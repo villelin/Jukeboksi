a7a66aac4e8cd63e536c7c884b1a1606f6aa8e7c
Puutarhatonttu\Mikael <mikaelan@metropolia.fi>
2017-05-03 15:00:18 +0300

    discreader

--- src/blockbusters/jukeboksi/DiscReader.java
+++ src/blockbusters/jukeboksi/DiscReader.java
@@ -2,6 +2,7 @@ package blockbusters.jukeboksi;
 
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.Color;
 
 public class DiscReader {
 
@@ -14,4 +15,43 @@ public class DiscReader {
 		this.discReader = discReader;
 		this.discPlayer = discPlayer;
 	}
+	
+	public void nextSong() {
+		discPlayer.rotate(-90);
+	}
+	
+	public void lastSong() {
+		discPlayer.rotate(90);
+	}
+	
+	public int getSongID() {
+		
+		int song = 0;
+		
+		switch (discReader.getColorID()) 
+		{
+			case Color.GREEN:
+				song = 1;
+				break;
+				
+				
+			case Color.RED:
+				song = 2;
+				break;
+				
+			case Color.BLUE:
+				song = 3;
+				break;
+				
+			case Color.WHITE:
+				song = 4;
+				break;
+				
+			default:
+				song = -1;
+				break;
+		}
+
+		return 0;
+	}	
 }

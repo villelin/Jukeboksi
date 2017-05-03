b7461ffc42144991ca185679b817f5427535f080
villelin <villelin@metropolia.fi>
2017-05-03 13:33:11 +0300

    parempi interface MusicPlayeriin

--- src/blockbusters/jukeboksi/MusicPlayer.java
+++ src/blockbusters/jukeboksi/MusicPlayer.java
@@ -4,31 +4,45 @@ import lejos.hardware.Sound;
 
 public class MusicPlayer extends Thread
 {
-	private MusicTrack[] music;
+	private MusicTrack[] songs;
+	private MusicTrack current_song;
 	
-	private boolean running;
+	private boolean playing;
 	private int volume;
-	
-	private int musicnum;
-	private int nextmusic;
-	
-	private MusicTrack current_music;
 	
 	public MusicPlayer()
 	{
-		current_music = null;
+		songs = new MusicTrack[6];
+		songs[0] = new Mario();
+		songs[1] = new Zelda();
+		songs[2] = new Tetris();
+		songs[3] = new JohnCena();
+		songs[4] = new EpicSax();
+		songs[5] = new Trololol();
+		
+		current_song = songs[0];
+		playing = false;
+		volume = 10;
 	}
 	
-	public void stop_playing()
+	public void startPlaying()
 	{
-		running = false;
+		playing = true;
+		current_song.restart();
 	}
 	
-	public void change_music(int music)
+	public void stopPlaying()
 	{
-		if (music != musicnum)
+		playing = false;
+	}
+	
+	public void changeSong(int song)
+	{
+		if (song >= 0 && song < songs.length)
 		{
-			nextmusic = music;
+			current_song = songs[song];
+			current_song.restart();
+			playing = false;
 		}
 	}
 	
@@ -50,64 +64,40 @@ public class MusicPlayer extends Thread
 	
 	public String getSongName()
 	{
-		if (current_music != null)
-			return current_music.getSongName();
+		if (current_song != null)
+			return current_song.getSongName();
 		else
 			return "";
 	}
 	
 	public String getArtistName()
 	{
-		if (current_music != null)
-			return current_music.getArtistName();
+		if (current_song != null)
+			return current_song.getArtistName();
 		else
 			return "";
 	}
 	
 	public void run()
 	{
-		music = new MusicTrack[6];
-		music[0] = new Mario();
-		music[1] = new Zelda();
-		music[2] = new Tetris();
-		music[3] = new JohnCena();
-		music[4] = new EpicSax();
-		music[5] = new Trololol();
+		Sound.setVolume(volume);
 		
-		volume = 10;
-		Sound.setVolume(10);
-		
-		current_music = music[0];
-		
-		running = true;
-		
-		musicnum = 0;
-		nextmusic = 0;
-		
-		while (running)
+		while (true)
 		{
-			if (nextmusic != musicnum)
+			if (playing)
 			{
-				if (nextmusic >= 0 && nextmusic < music.length)
-				{
-					current_music = music[nextmusic];
-					current_music.restart();
-					musicnum = nextmusic;
-				}
+				int[] notedata = current_song.getNote();
+			
+				int freq = notedata[0];
+				int note_duration = notedata[1];
+				int note_plus_pause = notedata[2];
+
+				Sound.playNote(Sound.XYLOPHONE, freq, note_duration);
+			
+				Sound.pause(note_plus_pause - note_duration);
+			
+				current_song.advance();
 			}
-			
-			int[] notedata = current_music.getNote();
-			
-			int freq = notedata[0];
-			int note_duration = notedata[1];
-			int note_plus_pause = notedata[2];
-			
-			//Sound.playTone(freq, note_duration);
-			Sound.playNote(Sound.XYLOPHONE, freq, note_duration);
-			
-			Sound.pause(note_plus_pause - note_duration);
-			
-			current_music.advance();
 		}
 	}
 }

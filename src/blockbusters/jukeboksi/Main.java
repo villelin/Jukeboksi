c43eee8ead7d333c590baf191cee81fce6e901b5
villelin <villelin@metropolia.fi>
2017-05-05 09:45:37 +0300

    asd

--- src/blockbusters/jukeboksi/Main.java
+++ src/blockbusters/jukeboksi/Main.java
@@ -2,13 +2,9 @@ package blockbusters.jukeboksi;
 
 public class Main {
 	
-	private int jonne;
-	private int mulu;
-	
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JukeBoksi jukebox = new JukeBoksi();
+		jukebox.Player();
 	}
 
 }
